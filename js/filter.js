4f19a216c0d657e0a61e124e0e2cae100345e2cd
Yulia Elagina <julia-elagina@yandex.ru>
2020-02-17 12:01:59 +0300

    Возвращает функционал генерирования меток обратно в модуль карты

--- js/filter.js
+++ js/filter.js
@@ -3,14 +3,7 @@
 (function () {
   var filterElement = document.querySelector('.map__filters');
   var filterFields = filterElement.querySelectorAll('select', 'fieldset');
-  var advertisements = window.data.list;
 
-  var showSimilarPins = function () {
-    var pinListElement = document.querySelector('.map__pins');
-    var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-    var fragment = window.similarPins.create(advertisements, mapPinTemplate);
-    pinListElement.appendChild(fragment);
-  };
 
   var activateSelects = function () {
     filterFields.forEach(function (el) {
@@ -26,7 +19,6 @@
 
   window.filter = {
     activate: activateSelects,
-    disable: disableSelects,
-    showPins: showSimilarPins
+    disable: disableSelects
   };
 })();
--- js/map.js
+++ js/map.js
@@ -2,11 +2,20 @@
 
 (function () {
   var mapElement = document.querySelector('.map');
+  var advertisements = window.data.list;
+
+  var showSimilarPins = function () {
+    var pinListElement = document.querySelector('.map__pins');
+    var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+    var fragment = window.similarPins.create(advertisements, mapPinTemplate);
+    pinListElement.appendChild(fragment);
+  };
+
 
   var activateMap = function () {
     mapElement.classList.remove('map--faded');
     window.filter.activate();
-    window.filter.showPins();
+    showSimilarPins();
   };
 
   var disableMap = function () {
